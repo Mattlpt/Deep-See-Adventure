9dc92e04ea143804bf310ea24d2530a80a0e104b
Benjamin Bordes <benjaminbordes78@gmail.com>
2024-03-19 23:23:17 +0100

    config tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config}*/
+export default {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+
+    theme: {
+        extend: {},
+    },
+
+    plugins: [],
+}
+
+const config = {
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+
+    theme: {
+        extend: {},
+    },
+
+    plugins: [],
+}
+
+module.exports = config
